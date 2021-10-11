ac0328b3483c2b903b4551ed31258a11c1eb751b
Donirk <donirk@pop-os.localdomain>
2021-10-11 12:49:10 +0200

    👿I hate bad documentations

---
+++ components/Teaser.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    v-editable="blok"
+    class="py-8 mb-6 text-5xl font-bold text-center">
+    <img class="h-48 w-full mb-4 object-cover" :src="blok.image.filename" :alt="blok.image.alt"/>
+    {{ blok.headline }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
